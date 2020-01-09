4642432e44389ad9a5f52de1ca02f5a1802b86ee
Ilia Lesik <ilialesik@gmail.com>
2020-01-09 16:34:21 +0300

    Create developers icons components

--- src/components/icons/index.stories.tsx
+++ src/components/icons/index.stories.tsx
@@ -18,6 +18,46 @@ import {Person} from "./Person";
 import {Eye} from "./Eye";
 import {EyeClosed} from "./EyeClosed";
 import {Jersey} from "./Jersey";
+import {Code} from "./Code";
+import {DiffAdded} from "./DiffAdded";
+import {DiffIgnored} from "./DiffIgnored";
+import {Gist} from "./Gist";
+import {DiffModified} from "./DiffModified";
+import {DiffRemoved} from "./DiffRemoved";
+import {GitPullRequest} from "./GitPullRequest";
+import {IssueReopened} from "./IssueReopened";
+import {NoNewLine} from "./NoNewLine";
+import {Broadcast} from "./Broadcast";
+import {Keyboard} from "./Keyboard";
+import {FileZip} from "./FileZip";
+import {FileBinary} from "./FileBinary";
+import {FileCode} from "./FileCode";
+import {FileDirectory} from "./FileDirectory";
+import {FileMedia} from "./FileMedia";
+import {FilePdf} from "./FilePdf";
+import {FileSubmodule} from "./FileSubmodule";
+import {FileSymlinkDirectory} from "./FileSymlinkDirectory";
+import {FileSymlinkFile} from "./FileSymlinkFile";
+import {Browser} from "./Browser";
+import {File} from "./File";
+import {RepoClone} from "./RepoClone";
+import {RepoForcePush} from "./RepoForcePush";
+import {RepoForked} from "./RepoForked";
+import {RepoPull} from "./RepoPull";
+import {RepoPush} from "./RepoPush";
+import {Repo} from "./Repo";
+import {Ruby} from "./Ruby";
+import {Mirror} from "./Mirror";
+import {Server} from "./Server";
+import {Terminal} from "./Terminal";
+import {RadioTower} from "./RadioTower";
+import {Rss} from "./Rss";
+import {Versions} from "./Versions";
+import {Dependent} from "./Dependent";
+import {RepoTemplate} from "./RepoTemplate";
+import {RepoTemplatePrivate} from "./RepoTemplatePrivate";
+import {Workflow} from "./Workflow";
+import {WorkflowAll} from "./WorkflowAll";
 
 
 storiesOf('Icons', module)
@@ -38,4 +78,44 @@ storiesOf('Icons', module)
     .add('Person', () => <Person/>)
     .add('Eye', () => <Eye/>)
     .add('EyeClosed', () => <EyeClosed/>)
-    .add('Jersey', () => <Jersey/>);
+    .add('Jersey', () => <Jersey/>)
+    .add('Code', () => <Code/>)
+    .add('DiffAdded', () => <DiffAdded/>)
+    .add('DiffIgnored', () => <DiffIgnored/>)
+    .add('Gist', () => <Gist/>)
+    .add('DiffModified', () => <DiffModified/>)
+    .add('DiffRemoved', () => <DiffRemoved/>)
+    .add('GitPullRequest', () => <GitPullRequest/>)
+    .add('IssueReopened', () => <IssueReopened/>)
+    .add('NoNewLine', () => <NoNewLine/>)
+    .add('Broadcast', () => <Broadcast/>)
+    .add('Keyboard', () => <Keyboard/>)
+    .add('FileZip', () => <FileZip/>)
+    .add('FileBinary', () => <FileBinary/>)
+    .add('FileCode', () => <FileCode/>)
+    .add('FileDirectory', () => <FileDirectory/>)
+    .add('FileMedia', () => <FileMedia/>)
+    .add('FilePdf', () => <FilePdf/>)
+    .add('FileSubmodule', () => <FileSubmodule/>)
+    .add('FileSymlinkDirectory', () => <FileSymlinkDirectory/>)
+    .add('FileSymlinkFile', () => <FileSymlinkFile/>)
+    .add('Browser', () => <Browser/>)
+    .add('File', () => <File/>)
+    .add('RepoClone', () => <RepoClone/>)
+    .add('RepoForcePush', () => <RepoForcePush/>)
+    .add('RepoForked', () => <RepoForked/>)
+    .add('RepoPull', () => <RepoPull/>)
+    .add('RepoPush', () => <RepoPush/>)
+    .add('Repo', () => <Repo/>)
+    .add('Ruby', () => <Ruby/>)
+    .add('Mirror', () => <Mirror/>)
+    .add('Server', () => <Server/>)
+    .add('Terminal', () => <Terminal/>)
+    .add('RadioTower', () => <RadioTower/>)
+    .add('Rss', () => <Rss/>)
+    .add('Versions', () => <Versions/>)
+    .add('Dependent', () => <Dependent/>)
+    .add('RepoTemplate', () => <RepoTemplate/>)
+    .add('RepoTemplatePrivate', () => <RepoTemplatePrivate/>)
+    .add('Workflow', () => <Workflow/>)
+    .add('WorkflowAll', () => <WorkflowAll/>);
--- src/frames/styleguide-3-0/Styleguide30.tsx
+++ src/frames/styleguide-3-0/Styleguide30.tsx
@@ -18,6 +18,56 @@ import {Person} from "../../components/icons/Person";
 import {Eye} from "../../components/icons/Eye";
 import {EyeClosed} from "../../components/icons/EyeClosed";
 import {Jersey} from "../../components/icons/Jersey";
+import {Code} from "../../components/icons/Code";
+import {DiffAdded} from "../../components/icons/DiffAdded";
+import {DiffIgnored} from "../../components/icons/DiffIgnored";
+import {Gist} from "../../components/icons/Gist";
+import {DiffModified} from "../../components/icons/DiffModified";
+import {DiffRemoved} from "../../components/icons/DiffRemoved";
+import {GitCommit} from "../../components/icons/GitCommit";
+import {DiffRenamed} from "../../components/icons/DiffRenamed";
+import {GitCompare} from "../../components/icons/GitCompare";
+import {Diff} from "../../components/icons/Diff";
+import {GitBranch} from "../../components/icons/GitBranch";
+import {GitMerge} from "../../components/icons/GitMerge";
+import {Database} from "../../components/icons/Database";
+import {GitPullRequest} from "../../components/icons/GitPullRequest";
+import {IssueClosed} from "../../components/icons/IssueClosed";
+import {IssueOpened} from "../../components/icons/IssueOpened";
+import {IssueReopened} from "../../components/icons/IssueReopened";
+import {CircuitBoard} from "../../components/icons/CircuitBoard";
+import {NoNewLine} from "../../components/icons/NoNewLine";
+import {Broadcast} from "../../components/icons/Broadcast";
+import {Keyboard} from "../../components/icons/Keyboard";
+import {FileZip} from "../../components/icons/FileZip";
+import {FileBinary} from "../../components/icons/FileBinary";
+import {FileCode} from "../../components/icons/FileCode";
+import {FileDirectory} from "../../components/icons/FileDirectory";
+import {FileMedia} from "../../components/icons/FileMedia";
+import {FilePdf} from "../../components/icons/FilePdf";
+import {FileSubmodule} from "../../components/icons/FileSubmodule";
+import {FileSymlinkDirectory} from "../../components/icons/FileSymlinkDirectory";
+import {FileSymlinkFile} from "../../components/icons/FileSymlinkFile";
+import {Browser} from "../../components/icons/Browser";
+import {File} from "../../components/icons/File";
+import {RepoClone} from "../../components/icons/RepoClone";
+import {RepoForcePush} from "../../components/icons/RepoForcePush";
+import {RepoForked} from "../../components/icons/RepoForked";
+import {RepoPull} from "../../components/icons/RepoPull";
+import {RepoPush} from "../../components/icons/RepoPush";
+import {Repo} from "../../components/icons/Repo";
+import {Ruby} from "../../components/icons/Ruby";
+import {Mirror} from "../../components/icons/Mirror";
+import {Server} from "../../components/icons/Server";
+import {Terminal} from "../../components/icons/Terminal";
+import {RadioTower} from "../../components/icons/RadioTower";
+import {Rss} from "../../components/icons/Rss";
+import {Versions} from "../../components/icons/Versions";
+import {Dependent} from "../../components/icons/Dependent";
+import {RepoTemplate} from "../../components/icons/RepoTemplate";
+import {RepoTemplatePrivate} from "../../components/icons/RepoTemplatePrivate";
+import {Workflow} from "../../components/icons/Workflow";
+import {WorkflowAll} from "../../components/icons/WorkflowAll";
 
 const styles = StyleSheet.create({
     frame: {
@@ -29,7 +79,9 @@ const styles = StyleSheet.create({
     iconsLine: {
         flexDirection: "row",
         marginTop: 40,
-        alignItems: "center"
+        alignItems: "center",
+        maxWidth: 1040,
+        flexWrap: "wrap"
     },
     iconMargin: {
         marginRight: 20
@@ -50,7 +102,7 @@ export const Styleguide30 = (props) => {
                 <Octoface style={styles.iconMargin} />
                 <Paintcan style={styles.iconMargin} />
             </View>
-            <View style={{marginTop: 40}}>
+            <View style={{marginTop: 80}}>
                 <StyleguideSeparatorWrapper>
                     <StyleguideLabel text="People" />
                 </StyleguideSeparatorWrapper>
@@ -66,6 +118,64 @@ export const Styleguide30 = (props) => {
                 <Eye style={styles.iconMargin} />
                 <EyeClosed style={styles.iconMargin} />
                 <Jersey style={styles.iconMargin} />
+            </View>
+            <View style={{marginTop: 80}}>
+                <StyleguideSeparatorWrapper>
+                    <StyleguideLabel text="Developer" />
+                </StyleguideSeparatorWrapper>
+            </View>
+            <View style={styles.iconsLine}>
+                <Code style={styles.iconMargin} />
+                <DiffAdded style={styles.iconMargin} />
+                <DiffIgnored style={styles.iconMargin} />
+                <Gist style={styles.iconMargin} />
+                <DiffModified style={styles.iconMargin} />
+                <DiffRemoved style={styles.iconMargin} />
+                <GitCommit style={styles.iconMargin} />
+                <DiffRenamed style={styles.iconMargin} />
+                <GitCompare style={styles.iconMargin} />
+                <Diff style={styles.iconMargin} />
+                <GitBranch style={styles.iconMargin} />
+                <GitMerge style={styles.iconMargin} />
+                <Database style={styles.iconMargin} />
+                <GitPullRequest style={styles.iconMargin} />
+                <IssueClosed style={styles.iconMargin} />
+                <IssueOpened style={styles.iconMargin} />
+                <IssueReopened style={styles.iconMargin} />
+                <CircuitBoard style={styles.iconMargin} />
+                <NoNewLine style={styles.iconMargin} />
+                <Broadcast style={styles.iconMargin} />
+                <Keyboard style={styles.iconMargin} />
+                <FileZip style={styles.iconMargin} />
+                <FileBinary style={styles.iconMargin} />
+                <FileBinary style={styles.iconMargin} />
+                <FileCode style={styles.iconMargin} />
+                <FileDirectory style={styles.iconMargin} />
+                <FileMedia style={styles.iconMargin} />
+                <FilePdf style={styles.iconMargin} />
+                <FileSubmodule style={styles.iconMargin} />
+                <FileSymlinkDirectory style={styles.iconMargin} />
+                <FileSymlinkFile style={styles.iconMargin} />
+                <Browser style={styles.iconMargin} />
+                <File style={styles.iconMargin} />
+                <RepoClone style={styles.iconMargin} />
+                <RepoForcePush style={styles.iconMargin} />
+                <RepoForked style={styles.iconMargin} />
+                <RepoPull style={styles.iconMargin} />
+                <RepoPush style={styles.iconMargin} />
+                <Repo style={styles.iconMargin} />
+                <Ruby style={styles.iconMargin} />
+                <Mirror style={styles.iconMargin} />
+                <Server style={styles.iconMargin} />
+                <Terminal style={styles.iconMargin} />
+                <RadioTower style={styles.iconMargin} />
+                <Rss style={styles.iconMargin} />
+                <Versions style={styles.iconMargin} />
+                <Dependent style={styles.iconMargin} />
+                <RepoTemplate style={styles.iconMargin} />
+                <RepoTemplatePrivate style={styles.iconMargin} />
+                <Workflow style={styles.iconMargin} />
+                <WorkflowAll style={styles.iconMargin} />
             </View>
         </View>
     </Frame>
